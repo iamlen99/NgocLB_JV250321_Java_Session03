25c66c5e4bdf7e8a5f461617616bebf198230f88
iamlen99 <allblue2903@gmail.com>
2025-06-25 11:47:42 +0700

    Upload bài tập MD4 session03_01

---
+++ TPLUS_JV250321_MD04_Session03_Bai01/src/FindMax.java
@@ -0,0 +1,30 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class FindMax {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Nhap so phan tu cua mang:");
+        int number = Integer.parseInt(sc.nextLine());
+        int[] intsArr = new int[number];
+        System.out.println("Nhap gia tri cac phan tu:");
+        for (int i = 0; i < number; i++) {
+            intsArr[i] = sc.nextInt();
+        }
+        System.out.println(Arrays.toString(intsArr));
+
+        for (int i = 0; i < intsArr.length; i++) {
+            int maxIndex = i;
+            for (int j = i; j < intsArr.length; j++) {
+                if (intsArr[j] > intsArr[maxIndex]) {
+                    maxIndex = j;
+                }
+            }
+            int temp = intsArr[maxIndex];
+            intsArr[maxIndex] = intsArr[i];
+            intsArr[i] = temp;
+        }
+        System.out.println("Mang sau khi sap xep theo thu tu giam dan: " + Arrays.toString(intsArr));
+        System.out.println("Phan tu lon nhat trong mang la: " + intsArr[0]);
+    }
+}
